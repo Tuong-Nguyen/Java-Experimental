a4bee9003c02360b84fb1dbbebde63b470fd853a
Tuong Nguyen <nctuong@tma.com.vn>
2018-06-27 06:54:59 +0700

    Add getPage method

--- java-ssl/java-ssl/src/main/java/treeapp/java_ssl/App.java
+++ java-ssl/java-ssl/src/main/java/treeapp/java_ssl/App.java
@@ -3,25 +3,30 @@ package treeapp.java_ssl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+
+import com.google.common.io.CharStreams;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) throws MalformedURLException, IOException
-    {
-    	String url = "https://www.google.com";
+public class App {
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		String url = "https://www.google.com";
+		App app = new App();
+		String output = app.getPage(url);
+		System.out.println(output);
+	}
+
+	public String getPage(String url) throws IOException, MalformedURLException {
 		URLConnection connection = new URL(url).openConnection();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		
-		String line;
-		while((line = reader.readLine()) != null) {
-			System.out.println(line);
-		}
-    }
+		String output = CharStreams.toString(reader);
+		reader.close();
+		return output;
+	}
 }
--- java-ssl/java-ssl/src/test/java/treeapp/java_ssl/AppTest.java
+++ java-ssl/java-ssl/src/test/java/treeapp/java_ssl/AppTest.java
@@ -1,43 +1,40 @@
 package treeapp.java_ssl;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+import org.hamcrest.Matchers
+;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest{
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-    	try {
-			App.main(null);
+	@Test	
+	public void getPage_UntrustedCertificate_ThrowException() {
+		App app = new App();
+		String url = "https://self-signed.badssl.com";
+		
+		try {
+			app.getPage(url);
+			fail();
 		} catch (Exception e) {
-			e.printStackTrace();
 		}
-        assertTrue( true );
-    }
+	}
+	
+	@Test	
+	public void getPage_TrustedCertificate_DoNotThrowException() throws Exception {
+		App app = new App();
+		String url = "https://www.google.com";
+		
+
+String output = 			app.getPage(url);
+		
+assertThat(output, not(isEmptyString()));
+	}
 }
