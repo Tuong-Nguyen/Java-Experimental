5d43ab98974e87f3fb68e8edeea3a4ace82c5cdc
Tuong Nguyen <nctuong@tma.com.vn>
2018-06-27 22:49:19 +0700

    correct the test

--- java-ssl/java-ssl/src/test/java/treeapp/java_ssl/AppTest.java
+++ java-ssl/java-ssl/src/test/java/treeapp/java_ssl/AppTest.java
@@ -1,40 +1,36 @@
 package treeapp.java_ssl;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 import org.junit.Test;
-import org.hamcrest.Matchers
-;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Unit test for simple App.
  */
 
-public class AppTest{
+public class AppTest {
 
-	@Test	
+	@Test
 	public void getPage_UntrustedCertificate_ThrowException() {
 		App app = new App();
 		String url = "https://self-signed.badssl.com";
-		
+
 		try {
 			app.getPage(url);
 			fail();
 		} catch (Exception e) {
 		}
 	}
-	
-	@Test	
+
+	@Test
 	public void getPage_TrustedCertificate_DoNotThrowException() throws Exception {
 		App app = new App();
 		String url = "https://www.google.com";
-		
 
-String output = 			app.getPage(url);
-		
-assertThat(output, not(isEmptyString()));
+		String output = app.getPage(url);
+
+		assertThat(output, not(isEmptyString()));
 	}
 }
